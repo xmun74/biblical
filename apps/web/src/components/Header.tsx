635bc343b0e445421c42104703c4035d297a8728
moon <xmun74@naver.com>
2023-07-12 17:05:06 +0900

    feat(@biblical/web): header component, 분기, logout api 틀

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,12 +1,62 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const URL = process.env.API_URL;
+  const isLoggedId = true;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const handleMenuClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  const handleLogout = async () => {
+    try {
+      const data = await axios.get(`${URL}/auth/logout`);
+      console.log('로그아웃', data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="p-6">
-      <Link to="/">홈 </Link>
-      <Link to="/auth/login">로그인 </Link>
-      <Link to="/auth/signup">회원가입</Link>
-    </div>
+    <header className="flex justify-between items-center sticky top-0 z-20 h-[80px] px-[40px] bg-white/50 backdrop-blur lg:mx-auto lg:max-w-[1256px]">
+      <div className="flex text-xl">
+        <Link to="/" className="font-monda mr-10">
+          Biblical
+        </Link>
+        <Link to="/" className="font-extrabold mr-6 hover:text-accent-350">
+          성경읽기
+        </Link>
+        <Link to="/" className="font-extrabold hover:text-accent-300">
+          모임
+        </Link>
+      </div>
+      {isLoggedId ? (
+        <div className="relative flex items-center">
+          <Link to="/auth/login" className="font-bold text-accent-400 mr-4">
+            MY 성경기록
+          </Link>
+          <div className="bg-slate-500 w-9 h-9 border rounded-3xl cursor-pointer" onClick={handleMenuClick}></div>
+          {isModalOpen && (
+            <div className="w-[184px] absolute right-0 z-20 top-[40px] p-2 rounded shadow-lg border">
+              <Link to={`/users/:$id`} className="block px-4 py-2 hover:bg-slate-50">
+                내 정보 수정
+              </Link>
+              <div className="px-4 py-2 hover:bg-slate-50 cursor-pointer" onClick={handleLogout}>
+                로그아웃
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="text-sm text-slate-600">
+          <Link to="/auth/login" className="border-r border-slate-200 pr-2 mr-2">
+            로그인
+          </Link>
+          <Link to="/auth/signup">회원가입</Link>
+        </div>
+      )}
+    </header>
   );
 };
 export default Header;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -9,12 +9,14 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        monda: ['Monda', 'sans-serif'],
       },
       colors: {
         accent: {
           100: 'rgba(184, 255, 217)',
           200: 'rgba(104, 255, 174)',
           300: 'rgba(3, 254, 118)',
+          350: 'rgba(20, 247, 117)',
           400: 'rgba(46, 230, 131)',
           500: 'rgba(29, 193, 104)',
           600: 'rgba(14, 132, 68)',
