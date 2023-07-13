2070502b0aa3ba16860fa817fc7d8b1b0e7bbab9
moon <xmun74@naver.com>
2023-07-13 22:24:27 +0900

    chore: kakaologin url

--- apps/web/src/components/KakaoLoginBtn.tsx
+++ apps/web/src/components/KakaoLoginBtn.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 
 const KakaoLoginBtn = () => {
-  const KAKAO_AUTH_URL = `/auth/kakao`;
+  const KAKAO_AUTH_URL = `${process.env.API_URL}/auth/kakao`;
   return (
-    <Link to={`${KAKAO_AUTH_URL}`}>
+    <Link to={`${KAKAO_AUTH_URL}`} target="_blank" rel="noreferrer">
       <button className="bg-kakao-login w-[210px] h-[51px] bg-no-repeat bg-cover mt-2" />
     </Link>
   );
